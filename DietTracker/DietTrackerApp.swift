a33992959dce9c11918e0de77d7892c8dc563f5f
KimmyWingz <127132791+fishpeopleapps@users.noreply.github.com>
2024-02-25 17:08:54 -0700

    initial commit

---
+++ DietTracker/DietTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DietTrackerApp.swift
+//  DietTracker
+//
+//  Created by Kimberly Brewer on 2/25/24.
+//
+
+import SwiftUI
+
+@main
+struct DietTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
